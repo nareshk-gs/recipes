11f868130ca5509d7bf59216d563955372473ece
Naresh Gourishetty <nareshk.gs@gmail.com>
2019-06-26 16:16:47 -0400

    add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (categoryName) VALUES ('American');
+INSERT INTO category (categoryName) VALUES ('Italian');
+INSERT INTO category (categoryName) VALUES ('Mexican');
+INSERT INTO category (categoryName) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
