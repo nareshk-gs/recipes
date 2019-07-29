8511f63a575be85b27f441eac35e69d5150e678f
Naresh Gourishetty <nareshk.gs@gmail.com>
2019-07-29 11:29:21 -0400

    sle4j logging on controller

--- src/main/java/org/sundry/recipes/controllers/IndexController.java
+++ src/main/java/org/sundry/recipes/controllers/IndexController.java
@@ -4,14 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.sundry.recipes.model.Category;
-import org.sundry.recipes.model.UnitOfMeasure;
-import org.sundry.recipes.repositories.CategoryRepository;
-import org.sundry.recipes.repositories.UnitOfMeasureRepository;
 import org.sundry.recipes.service.RecipeService;
-
-import javax.swing.text.html.Option;
-import java.util.Optional;
 
 /**
  * Created by kon1299 on 2019-03-19
